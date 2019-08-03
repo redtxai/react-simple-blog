86c2f3ac1372a7f00af34639e2dac246d9287209
redtxai <redtxai@gmail.com>
2019-08-03 13:38:00 -0300

    fix(server) Fix server route

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const serveStatic = require("serve-static");
 const path = require("path");
 const app = express();
 app.use(serveStatic(path.join(__dirname, "build")));
-app.get("/", function(req, res) {
+app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 const port = process.env.PORT || 80;
