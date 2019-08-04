035bfc38cea7377cf63facb8797a9ac426a5017a
redtxai <redtxai@gmail.com>
2019-08-04 16:39:10 -0300

    feat(lateralnavbar) Add a caret for the trigger

--- src/components/LateralNavBar/LateralNavBar.jsx
+++ src/components/LateralNavBar/LateralNavBar.jsx
@@ -30,10 +30,25 @@ const LateralNavBarTrigger = styled.label`
   color: #6A6B6D;
   z-index: 11;
 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   &:hover {
     cursor: pointer;
     background-color: #EBECEE;
   }
+`
+
+const LateralNavbarCaret = styled.svg`
+  transform: rotate(${props => (props.show > 0 ? '0' : '180')}deg);
+
+  &:hover {
+    cursor: pointer;
+  }
+`
+const SortPath = styled.path`
+  fill: #6A6B6D;
 `
 
 class LateralNavBar extends Component {
@@ -47,7 +62,18 @@ class LateralNavBar extends Component {
   render() {
     return (
       <LateralNavBarRoot show={this.state.active}>
-        <LateralNavBarTrigger onClick={ () => this.setState({active: !this.state.active}) }/>
+        <LateralNavBarTrigger onClick={ () => this.setState({active: !this.state.active}) }>
+          <LateralNavbarCaret show={this.state.active}
+            enable-background="new 0 0 32 32"
+            height="20x"
+            id="Layer_1"
+            version="1.1"
+            viewBox="0 0 32 32"
+            width="20x"
+            xmlns="http://www.w3.org/2000/svg">
+              <SortPath d="M24.291,14.276L14.705,4.69c-0.878-0.878-2.317-0.878-3.195,0l-0.8,0.8c-0.878,0.877-0.878,2.316,0,3.194  L18.024,16l-7.315,7.315c-0.878,0.878-0.878,2.317,0,3.194l0.8,0.8c0.878,0.879,2.317,0.879,3.195,0l9.586-9.587  c0.472-0.471,0.682-1.103,0.647-1.723C24.973,15.38,24.763,14.748,24.291,14.276z"/>
+          </LateralNavbarCaret>
+        </LateralNavBarTrigger>
         <LateralNavBarContent selectPost={ () => this.setState({active: !this.state.active}) }/>
       </LateralNavBarRoot>
     )
